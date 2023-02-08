adbffd086d11b67642e6871640e28f5ee29dc628
Anton Filippov <derevenchshina@yandex.ru>
2023-02-08 11:16:52 +0500

    v1 comit without adding (no hooks)

---
+++ src/styles/ColorsConst.tsx
@@ -0,0 +1,28 @@
+export const ColorSchema = {
+    mainFont: '#eae7dc',
+    subFont: '#d8c3a5',
+    barFont: '#e85a4f',
+    objectFont: '#e98074',
+    textFont: '#eae7dc',
+    navBarFont: '#8e8d8a'
+}
+
+
+export const ColorSchema2 = {
+    mainFont: '#eae7dc',
+    subFont: '#d8c3a5',
+    barFont: '#e85a4f',
+    objectFont: '#e98074',
+    textFont: '#eae7dc',
+    navBarFont: '#8e8d8a'
+}
+
+
+export const ColorSchema3 = {
+    mainFont: '#eae7dc',
+    subFont: '#d8c3a5',
+    barFont: '#e85a4f',
+    objectFont: '#e98074',
+    textFont: '#eae7dc',
+    navBarFont: '#8e8d8a'
+}
